504176f268bb542fb8fc0b6a75968d42feea7be8
pink <aysunqotur@gmail.com>
2026-06-09 14:58:52 +0400

    reqabet bitdi

--- js/pages/government-relations.js
+++ js/pages/government-relations.js
@@ -23,3 +23,32 @@ function closeModalOutside(e) {
 document.addEventListener('keydown', (e) => {
   if (e.key === 'Escape') closeModal();
 });
+
+// RƏQABƏT MODALİ
+function openReqabetModal() {
+  scrollY = window.scrollY;
+  document.body.style.position = 'fixed';
+  document.body.style.top = `-${scrollY}px`;
+  document.body.style.width = '100%';
+  document.getElementById('reqabetModalOverlay').classList.add('active');
+}
+function closeReqabetModal() {
+  document.getElementById('reqabetModalOverlay').classList.remove('active');
+  document.body.style.position = '';
+  document.body.style.top = '';
+  document.body.style.width = '';
+  window.scrollTo(0, scrollY);
+}
+function closeReqabetModalOutside(event) {
+  if (event.target === document.getElementById('reqabetModalOverlay')) {
+    closeReqabetModal();
+  }
+}
+
+// ESC düyməsi
+document.addEventListener('keydown', function(e) {
+  if (e.key === 'Escape') {
+    closeSatinalmaModal();
+    closeReqabetModal();
+  }
+});
